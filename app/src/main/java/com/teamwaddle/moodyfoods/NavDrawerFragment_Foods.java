1c080a1c1a4fb95bfaf483307c35be66a08cd823
TeamWaddle <last0blivion28@gmail.com>
2015-01-21 22:16:50 -0600

    Initial commit

---
+++ app/src/main/java/com/teamwaddle/moodyfoods/NavDrawerFragment_Foods.java
@@ -0,0 +1,19 @@
+package com.teamwaddle.moodyfoods;
+
+/**
+ * Created by Google Fiber on 1/8/2015.
+ */
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class NavDrawerFragment_Foods {
+}
